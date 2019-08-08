fc9b601a7827d7e0dc50b14d3d99130da8212be0
shin hee moon <whale0091@gmail.com>
2019-08-08 17:53:45 +0900

    Add GameRoom,GameRoomManager class

---
+++ PlayGround/MainServer/GameRoom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainServer
+{
+    public class GameRoom
+    {
+        public GameRoom()
+        {
+
+        }
+    }
+
+    public class GameRoomManager
+    {
+        Dictionary<int, GameRoom> GameRoomDic = new Dictionary<int, GameRoom>();
+
+        public GameRoomManager()
+        {
+
+        }
+    }
+}
